86db4b9fcf6b61c792710e94534bcf0501d429b6
jgmontoya <jgmontoya@pm.me>
2019-08-21 19:17:34 -0400

    feat(api): initialize API and respond to basic query

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/recommend/<int:num_recommendations>', methods=['GET'])
+def recommend(num_recommendations):
+  #likes = request.args.getlist('likes', type=int)
+  #dislikes = request.args.getlist('dislikes', type=int)
+
+  return {'product_ids': [x for x in range(num_recommendations)]}
+
+if __name__ == '__main__':
+  app.run()
