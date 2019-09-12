707677ea847256d5ebe8fd6189470b867e40f2df
jgmontoya <jgmontoya@pm.me>
2019-09-12 00:54:22 -0300

    feat(recommend): filter displayed products and create default endpoint

--- recommender_api/recommender_api.py
+++ recommender_api/recommender_api.py
@@ -1,16 +1,30 @@
 from flask import (
-  Blueprint,
-  current_app
+    Blueprint
 )
+from .models import db
+from .models.product import Product
+from .models.product_action import ProductAction
 
 bp = Blueprint('recommender_api', __name__)
 
 
+@bp.route('/recommend/<int:receiver_id>/<int:num_recommendations>', methods=['GET'])  # type: ignore
+def recommend(receiver_id: int, num_recommendations: int) -> dict:
+
+    displayed_products = [id[0] for id in db.session.query(ProductAction.id).filter(
+        ProductAction.receiver_id == receiver_id and ProductAction.action_type == 0
+    ).all()]
+    print(displayed_products)
+
+    result = [r.id for r in Product.query.all()
+              if r.id not in displayed_products][:num_recommendations]
+
+    return {'product_ids': result}
+
+
 @bp.route('/recommend/<int:num_recommendations>', methods=['GET'])  # type: ignore
-def recommend(num_recommendations: int) -> dict:
-    # likes = request.args.getlist('likes', type=int)
-    # dislikes = request.args.getlist('dislikes', type=int)
+def default_recommendation(num_recommendations: int) -> dict:
 
-    products = current_app.products
+    result = [r.id for r in Product.query.all()][:num_recommendations]
 
-    return {'product_ids': products.sample(num_recommendations).id.tolist()}
+    return {'product_ids': result}
