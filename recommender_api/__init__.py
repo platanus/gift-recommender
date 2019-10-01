71fa1746ce0a2b0c4a057897a63ad7369f3d9238
Javier <jgmontoya@pm.me>
2019-10-01 15:05:24 -0300

    refactor(init): move recommender initialization to be handled with app context

--- recommender_api/__init__.py
+++ recommender_api/__init__.py
@@ -23,11 +23,11 @@ def create_app() -> Flask:
 
     db.init_app(app)
 
-    app.model = RecommenderModel()
-    with app.app_context():
-        app.model.load_products()
-
     from . import recommender_api
     app.register_blueprint(recommender_api.bp)
+
+    with app.app_context():
+        app.model = RecommenderModel()
+        app.model.load_products()
 
     return app
