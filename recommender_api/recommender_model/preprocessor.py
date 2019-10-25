8648683b5fbd324b4ec0446bf0caeeef37c77413
jgmontoya <jgmontoya@pm.me>
2019-10-25 16:09:07 -0300

    refactor(): compute product name vector in its own method

--- recommender_api/recommender_model/preprocessor.py
+++ recommender_api/recommender_model/preprocessor.py
@@ -14,15 +14,20 @@ class Preprocessor(object):
         self.vectors = KeyedVectors.load_word2vec_format(embeddings)
 
     def compute_vector(self, product) -> np.array:
-        tokens = product.name.lower().split()
-        vectors = np.array([self.get_vector(token) for token in tokens])
-        return np.average(vectors, axis=0)
+        name_vectors = self.compute_name_vector(product)
+
+        return name_vectors
 
     def get_vector(self, word: str) -> np.array:
         try:
             return self.vectors.get_vector(word)
         except KeyError:
             return np.zeros(self.vectors.vector_size)
+
+    def compute_name_vector(self, product) -> np.array:
+        tokens = product.name.lower().split()
+        vectors = np.array([self.get_vector(token) for token in tokens])
+        return np.average(vectors, axis=0)
 
 
 if __name__ == '__main__':
